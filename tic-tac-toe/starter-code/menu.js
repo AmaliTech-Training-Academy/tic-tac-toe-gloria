7f8bedba1ebf95a688d2207d255395348fa9ad66
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-03-08 16:28:33 +0000

    worked on the menu page

--- tic-tac-toe/starter-code/menu.js
+++ tic-tac-toe/starter-code/menu.js
@@ -1,8 +1,20 @@
 const playerX = document.querySelector(".x-outline");
 const playerO = document.querySelector(".o-outline");
 
+const Players = document.querySelectorAll('player-outline');
+const playerPath = document.querySelectorAll("player-class")
+
 const newSoloGame = document.getElementById("new-solo-btn");
 const newMultiGame = document.getElementById("new-multiplayer-btn");
+
+Players.forEach(player=>{
+  player.addEventListener('click', ()=>{
+    Players.forEach(player => {
+      player.classList.remove('clicked')
+    });
+    player.classList.add('clicked');
+  })
+})
 
 playerX.addEventListener("click", () => {
   // Set the user state to "selected"
@@ -10,12 +22,13 @@ playerX.addEventListener("click", () => {
 });
 
 playerO.addEventListener("click", () => {
+  // playerO.classList.add("clicked")
   sessionStorage.setItem("oState", "o");
 });
 
-// Add an event listener to button 2
+
+
 newSoloGame.addEventListener("click", () => {
-  // Get the user state from session storage
   const xState = sessionStorage.getItem("xState");
   const oState = sessionStorage.getItem("oState");
 
@@ -24,7 +37,6 @@ newSoloGame.addEventListener("click", () => {
     // Navigate to a new page
     window.location.href = "./new-game-solo.html";
   } else {
-    // Navigate to the home page
     alert("select user");
   }
 });
@@ -33,10 +45,8 @@ newMultiGame.addEventListener('click', ()=>{
     const xMark = sessionStorage.getItem("xState");
     const oMark = sessionStorage.getItem("oState")
     if (xMark === "x" || oMark === "o") {
-        // Navigate to a new page
         window.location.href = "./new-game-multiplayer.html";
       } else {
-        // Navigate to the home page
         alert("select user");
       }
 })
--- tic-tac-toe/starter-code/new-game.js
+++ tic-tac-toe/starter-code/new-game.js
@@ -3,56 +3,49 @@ const cpu = document.querySelector(".cpu")
 const playerScore = document.querySelector(".player-score");
 const cpuScore = document.querySelector(".cpu-score")
 const tieScore = document.querySelector('.tie-score');
+const boxes = Array.from(document.querySelectorAll(".box"));
 
-const xPlayer = new Image();
-xPlayer.src = './assets/icon-x.svg'
-const oPlayer = new Image();
-oPlayer.src = './assets/icon-o.svg';
+const xPlayer = '<img src="../starter-code/assets/icon-x.svg">';
+const oPlayer = '<img src="../starter-code/assets/icon-o.svg">';
 
-
-
-const xOutline = new Image();
-xOutline.src = './assets/icon-x-outline.svg';
-const oOutline = new Image();
-oOutline.src = './assets/icon-o-outline.svg';
-
+const xOutline = '<img src="../starter-code/assets/icon-x-outline.svg">';
+const oOutline = '<img src="../starter-code/assets/icon-o-outline.svg">';
 
 const oText = 'O';
 const xText = 'X';
-
-const playerState = document.querySelector(".player-state");
-
-let boxes = Array.from(document.getElementsByClassName('box'));
 
 let currentPlayer = xPlayer;
 
 //create array with 9 indexes
 let spaces = Array(9).fill(null);
+const isGameActive = true
 
-//start game
-const startGame = ()=>{
-    boxes.forEach(box => box.addEventListener('click', boxClicked))
-}
+//winning condition
+const winningCondition = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [0,4,7],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
 
-const boxClicked = (e)=>{
-    const id = e.target.id;
-    //if spaces is not filled with id
+]
 
-    if(!spaces[id]){
-        spaces[id] = currentPlayer;
-        e.target.innerHtml = currentPlayer;
 
-        currentPlayer = currentPlayer === xPlayer ? oPlayer : xPlayer;
-    }
-}
 
+boxes.forEach((box, index) =>{
+    box.addEventListener('click', ()=> userAction(box, index));
+});
 
 //restart game
 const restartBtn = document.querySelector('.restart-button')
 const restart =() =>{
     spaces.fill(null)
     boxes.forEach(box =>{
-        box.innerText = ''
+        box.innerHTML = ''
     });
     currentPlayer = xPlayer;
 }
