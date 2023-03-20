1af9c9588292a859022c085f4a81a82a6124d39f
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-03-20 23:38:45 +0000

    woked on placing players mark in the multigame

--- tic-tac-toe/starter-code/multigame.js
+++ tic-tac-toe/starter-code/multigame.js
@@ -1,40 +1,52 @@
-// const p1Mark = document.querySelector(".player1-mark");
-// const p2Mark = document.querySelector(".player2-mark");
-
-// const xPlayer = '<img src="../starter-code/assets/icon-x.svg">';
-// const oPlayer = '<img src="../starter-code/assets/icon-o.svg">';
-
-// const xOutline = '<img src="../starter-code/assets/icon-x-outline.svg">';
-// const oOutline = '<img src="../starter-code/assets/icon-o-outline.svg">';
-
-// const player1Mark = sessionStorage.getItem("selectedMark");
-
-// const xWon = document.querySelector(".o-announcer");
-
-// const oWon = document.querySelector(".x-announcer");
-// const tie = document.querySelector(".tie-announcer");
-let oTurn;
-const xMark = "x";
-const oMark = "o";
 const xPlayer = '<img src="../starter-code/assets/icon-x.svg">';
 const oPlayer = '<img src="../starter-code/assets/icon-o.svg">';
+
+const board = document.querySelector(".board");
+const playersTurn = document.querySelector(".players-turn");
+playersTurn.innerHTML = xPlayer;
+const xClass = "xMark";
+const oClass = "oMark";
 const player1Mark = sessionStorage.getItem("selectedMark");
 const player2Mark = player1Mark === "x" ? "o" : "x";
-const boxes = Array.from(document.querySelectorAll(".box"));
-boxes.forEach((box) => {
-  box.addEventListener("click", handleClick, { once: true });
-});
+const boxes = document.querySelectorAll("[data-cell]");
 
 const handleClick = (e) => {
   const box = e.target;
-  const currentPlayer = player1Mark === "x" ? xMark : oMark;
+  const currentPlayer = playersTurn.innerHTML === xPlayer ? xClass : oClass;
+  console.log("clicked");
   placeMark(box, currentPlayer);
+  switchPlayers();
+  setBoxesHoverState();
 };
+
+const setBoxesHoverState = () => {
+  board.classList.remove(xClass);
+  board.classList.remove(oClass);
+
+  if (playersTurn.innerHTML === xPlayer) {
+    board.classList.add(xClass);
+  } else {
+    board.classList.add(oClass);
+  }
+};
+
+const startGame = () => {
+  boxes.forEach((box) => {
+    box.addEventListener("click", handleClick, { once: true });
+  });
+  setBoxesHoverState();
+};
+startGame();
+
 const placeMark = (box, currentPlayer) => {
   box.classList.add(currentPlayer);
 };
 
-const winningCondition = [
+const switchPlayers = () => {
+  playersTurn.innerHTML = playersTurn.innerHTML === xPlayer ? oPlayer : xPlayer;
+};
+
+const winningCombos = [
   [0, 1, 2],
   [0, 3, 6],
   [0, 4, 8],
@@ -45,3 +57,12 @@ const winningCondition = [
   [3, 4, 5],
   [6, 7, 8],
 ];
+
+const restartBtn = document.querySelector(".restart-button");
+const restartModal = document.querySelector(".restart-modale");
+
+const restartGame = () => {
+  console.log("hi");
+  restartModal.classList.add(show);
+};
+restartBtn.addEventListener("click", restartGame);
