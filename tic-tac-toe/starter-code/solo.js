8b85bd0083b9d43419b78c6a58f44d7a12603615
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-03-24 16:17:17 +0000

    worked on the game with cpu

--- tic-tac-toe/starter-code/solo.js
+++ tic-tac-toe/starter-code/solo.js
@@ -1,6 +1,8 @@
 const xPlayer = '<img src="../starter-code/assets/icon-x.svg">';
 const oPlayer = '<img src="../starter-code/assets/icon-o.svg">';
 
+// let mark = "";
+
 const board = document.querySelector(".board");
 const whoseTurn = document.querySelector(".players-turn");
 whoseTurn.innerHTML = xPlayer;
@@ -10,33 +12,47 @@ const boxes = Array.from(document.querySelectorAll(".box"));
 let nextPlayer = "",
   firstPlayer = "";
 
-// let currentPlayer = sessionStorage.getItem("selectedMark") === 'x' ? playerMark : computerMark
+const playerMark = sessionStorage.getItem("selectedMark"),
+  getPlayerMark = document.querySelector(".players-type");
+getPlayerMark.innerHTML = playerMark;
 
-// const playerMark = sessionStorage.getItem("selectedMark");
-// const getPlayerMark = document.querySelector(".players-type");
-// getPlayerMark.innerHTML = playerMark;
+const cpuMark = sessionStorage.getItem("cpuMark");
+const getcpuMark = document.querySelector(".cpu-type");
+getcpuMark.innerHTML = cpuMark;
 
-// const cpuMark = sessionStorage.getItem("cpuMark");
-// const getcpuMark = document.querySelector(".cpu-type");
-// getcpuMark.innerHTML = cpuMark;
+const tieModal = document.querySelector(".tie-announcer");
 
-// const playersMark = playerMark === "x" ? xPlayer : oPlayer;
-// const computerMark = cpuMark === "x" ? xPlayer : oPlayer;
+const winningCombos = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
 
 const handleClick = (id) => {
   if (boxes[id].innerHTML === "") {
     if (firstPlayer === "player") {
       placeMark(id, xPlayer);
+      // if(checkWin(xPlayer)) {
+      // }
     } else {
       placeMark(id, oPlayer);
+      console.log(oPlayer);
     }
   }
   setTimeout(() => {
     cpuPlay();
   }, 1000);
-
-  console.log(id);
+  checkForWin();
 };
+
+const whoWins = () => {};
+
 const startGame = () => {
   if (sessionStorage.getItem("selectedMark") === "x") {
     firstPlayer = "player";
@@ -57,13 +73,11 @@ window.onload = () => {
 
 const placeMark = (id, mark) => {
   boxes[id].innerHTML = mark;
-  //whoseTurn === xPlayer ? xPlayer : oPlayer
-  //place mark when user click
-  //place cpu mark when a box is selected
+  switchTurns(id);
 };
 
-const switchPlayers = () => {
-  whoseTurn.innerHTML = currentPlayer === xPlayer ? oPlayer : xPlayer;
+const switchTurns = (id) => {
+  whoseTurn.innerHTML = boxes[id].innerHTML === xPlayer ? oPlayer : xPlayer;
 };
 
 const cpuPlay = () => {
@@ -80,4 +94,50 @@ const cpuPlay = () => {
   const index = boxarr.indexOf(randomNum);
   const mark = sessionStorage.getItem("cpuMark") === "x" ? xPlayer : oPlayer;
   placeMark(index, mark);
+};
+
+const restartBtn = document.querySelector(".restart-button"),
+  restartModal = document.querySelector(".restart-modale"),
+  cancelRestart = document.querySelector(".cancel-restart"),
+  restart = document.querySelector(".confirm-restart");
+const main = document.querySelector("main");
+
+const restartNewGame = () => {
+  whoseTurn.innerHTML = xPlayer;
+  restartModal.classList.remove("show-restart-modal");
+  const boxesId = boxes.map((ele) => ele.id);
+  boxesId.map((id) => {
+    boxes[id].innerHTML = "";
+  });
+};
+
+const restartGame = () => {
+  main.classList.add("modal-background");
+  restartModal.classList.add("show-restart-modal");
+  cancelRestart.addEventListener("click", () => {
+    restartModal.classList.remove("show-restart-modal");
+  });
+  restart.addEventListener("click", restartNewGame);
+};
+
+restartBtn.addEventListener("click", restartGame);
+
+const check_win = (mark) => {
+  let wincombination = [];
+  return winningCombos.some((combination) => {
+    const winCombo = combination.every((id) => {
+      boxes[id].innerHTML === mark;
+      // console.log(boxes[id].innerHTML);
+    });
+    if (winCombo) {
+      wincombination = [...combination];
+    }
+    return winCombo;
+  });
+  // if (mark === xPlayer) {
+  //   winCombo.forEach(id => {
+  //     boxes[id]
+  //   });
+  // }
+  // return mark;
 };
